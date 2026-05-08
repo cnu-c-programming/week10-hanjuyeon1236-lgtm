e4e6e758b912f1ceea3c329ba20115dcbda2e736
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-05-08 09:57:47 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -14,7 +14,7 @@ union file_access_rights {
 int main() {
   union file_access_rights some_file = {0};
   some_file.rights.read = 1;
-  some_file.righrs.write = 0;
+  some_file.rights.write = 0;
   some_file.rights.execute = 1;
   printf("current mode: %u\n", some_file.int_value);
 }
